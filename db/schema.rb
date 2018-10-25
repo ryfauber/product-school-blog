0377586f537fb1add2d23bd49b5261b9c760bbea
Ryan Fauber <ryan.fauber@idinsight.org>
2018-10-25 02:32:25 +0000

    adding email to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181009025720) do
+ActiveRecord::Schema.define(version: 20181025022936) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 20181009025720) do
     t.string  "first_name"
     t.string  "last_name"
     t.integer "age"
+    t.string  "email"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email"
 
 end
